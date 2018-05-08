28b699d97b4dc5fa6485dc2b243bcf7bc8293979
ryukkuzakku <ajattzack@gmail.com>
2018-05-07 16:06:39 -1000

    Added database, and database functions

--- make_database.py
+++ make_database.py
@@ -1,4 +1,5 @@
 import requests
+import sqlite3
 from bs4 import BeautifulSoup
 import nltk
 
@@ -17,18 +18,45 @@ def tokenize_sentences(text):
     
 ##crawlers
 
+def seperate_sentences(text):
+    pass
+
 ##flips through whole book and grabs all text into a list
 def get_all_text(url):
+    create_database()
     soup = get_soup(url)
     page_text = get_body(soup)
     book_list = []
     
     if len(page_text) is not 2:
+        page_text = page_text.replace("\n", ". ")
         book_list.extend(tokenize_sentences(page_text))
+
+    i = 1
+    book_name = soup.title.string.replace("Ulukau: ", "")
+    for sentence in book_list:
+        insert_data(sentence, i, url, book_name)
+        i = i + 1
             
     return book_list
 
 
+def create_database():
+    db = sqlite3.connect('data/sentences.db')
+    cursor = db.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS sentences(id INTEGER PRIMARY KEY, id_in_book INTEGER,
+                    sentence TEXT, source_url TEXT, book_name TEXT)''')
+    db.commit()
+    db.close()
+
+def insert_data(sentence, number, source_url, book_name):
+    db = sqlite3.connect('data/sentences.db')
+    cursor = db.cursor()
+    cursor.execute('''INSERT INTO sentences(sentence, id_in_book, source_url, book_name)
+                                    VALUES(?,?,?,?)''', (sentence, number, source_url, book_name))
+    db.commit()
+    db.close()
+    
 '''
 TODO
 *Add add_to_database (Takes sentence and adds it to database, organizing it by number, url, book name and sentence itself)
